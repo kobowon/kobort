9e98bd9ab2d21b59075e26ee09f9ee837d0150e2
kobowon <ts3507@ajou.ac.kr>
2021-12-28 10:13:35 +0900

    update ner-inference post-processing

--- inference.py
+++ inference.py
@@ -2,7 +2,7 @@ import argparse
 import os
 import torch
 import numpy as np
-from utils import set_seed, make_ner_data
+from utils import set_seed, make_ner_data, return_ner_tagged_sentence_v2
 from dataset import NerDataset
 from transformers import RobertaForTokenClassification, BertTokenizer
 
@@ -20,7 +20,7 @@ def infer(args):
                                               cls_token='<s>',
                                               mask_token='<mask>',
                                              )
-    infer_texts = ["오늘 고보원은 9시에 티맥스로 출근했다", "티맥스 소프트는 매각 되었다"]
+    infer_texts = ["오늘 고보원은 9시에 티맥스로 출근했다", "티맥스 소프트는 매각 되었다", "세련미 가미한 공격 우리은행 박혜진, 임영희, 양지희는 공격에도 능하다.", "쿠션 중앙의 작은 구멍으로 파운데이션이 나오는데, 양 조절이 익숙해지려면 몇 번 사용해봐야 할듯 해요.” ★★★★★원선희 “보통 쿠션 제품은 퍼프에 너무 많은 양이 묻어나 퍼프가 쉽게 더러워지고 양 조절이 힘든 단점이 있잖아요.","신카이 마코토 감독ㆍ배우 한예리··· ‘너의 이름은’ 메가토크에서 만나요!", '깐쇼새우 만드는 법, 이연복 셰프 비법은 "튀김옷 반죽에 식용류"']
     
     #Build dataloader
     entity_label_list, inference_data = make_ner_data(file_path=None,
@@ -68,15 +68,31 @@ def infer(args):
     batch_num = preds_cpu.shape[0]
     seq_len = preds_cpu.shape[1]
     for batch_idx in range(batch_num):
+        print("***********************************************************")
+        origin_text = infer_texts[batch_idx]
         #원래 문장 <s> </s> 포함 안 된 문장
         original_tokens = inference_data[batch_idx]["sentence"].split()
-        original_tokens = ['<s>'] + original_tokens + ['</s>']
+        tokens = ['<s>'] + original_tokens + ['</s>']
         print(f"\n문장 {batch_idx}의 결과는 아래와 같습니다")
-        for token_idx in range(len(original_tokens)): #유효한 토큰들만 살펴보기 <pad> 제외
+        origin_tokens_labels = []
+        #original_tokens의 실질적인 토큰 범위 내에서 (special token 제외)
+        for token_idx in range(1,len(tokens)-1): #유효한 토큰들만 살펴보기 <pad> 제외
             #예측한 label id를 label로 변환
             pred_label_index = preds_cpu[batch_idx][token_idx]
-            pred_label_str = id2label[str(pred_label_index)]           
-            print(f'{token_idx}번째 토큰 "{original_tokens[token_idx]}"의 라벨은 "{pred_label_str}"입니다')   
+            pred_label_str = id2label[str(pred_label_index)]
+            origin_tokens_labels.append(pred_label_str)
+            print(f'{token_idx}번째 토큰 "{tokens[token_idx]}"의 라벨은 "{pred_label_str}"입니다')
+        print(f"\n원문장 : {origin_text}")
+        print(f"토크나이즈된 문장 : {original_tokens}")
+        print(f"토큰에 대한 라벨 리스트 : {origin_tokens_labels}") 
+        
+        ner_tagged_sentence, ner_tagged_id_list = return_ner_tagged_sentence_v2(origin_text, original_tokens,origin_tokens_labels)
+  
+        print(f'태깅 문장 : {ner_tagged_sentence}')
+        for item in ner_tagged_id_list:
+            start, end = item
+            print(f'entity 확인 : {origin_text[start:end+1]}')
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
